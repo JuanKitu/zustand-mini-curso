b0577f5c36234b29fa94a7645fe705a75f7e0544
kitu <juanmix.01@gmail.com>
2025-08-05 01:37:41 -0300

    "Fix typo in JiraPage state management description"

--- src/pages/02-objects/JiraPage.tsx
+++ src/pages/02-objects/JiraPage.tsx
@@ -9,7 +9,7 @@ export const JiraPage = () => {
   return (
     <>
       <h1>Tareas</h1>
-      <p>Manejo de estado con objectos de Zustand</p>
+      <p>Manejo de estado con objetos de Zustand</p>
       <hr />
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
