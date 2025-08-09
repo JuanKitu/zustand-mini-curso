7cd27f7adfab10d405a0dc937095649d508c025f
kitu <juanmix.01@gmail.com>
2025-08-09 18:09:07 -0300

    "Implement and utilize request interceptor to attach auth token in tesloApi; enhance error handling in AuthService; add checkStatus method for authentication verification"

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,14 +4,28 @@ import {AxiosError} from "axios";
 
 
 export class AuthService {
+    private static formatError(error: unknown): never {
+        if (error instanceof AxiosError) {
+            throw new Error(error.response?.data ?? 'An unexpected error occurred');
+        }
+        throw new Error('Unable to login');
+    }
 
     static async login(email: string, password: string) : Promise<LoginResponse> {
         try {
             const { data } = await tesloApi.post<LoginResponse>('/auth/login', {email, password});
             return data;
         } catch (error) {
-            if(error instanceof AxiosError) throw new Error(error.response?.data);
-            throw new Error('unabled to login');
+            this.formatError(error);
+        }
+    }
+    static async checkStatus():Promise<LoginResponse> {
+        try {
+            const {data} = await tesloApi.get<LoginResponse>('/auth/check-status');
+            return data
+
+        } catch (error) {
+            this.formatError(error);
         }
     }
 }
